188fb394ae43434a457558a92d3a063ddcd78891
Will Glynn <will@willglynn.com>
2025-05-22 23:12:39 -0500

    Fix sensor._UNDEF

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -147,12 +147,12 @@ CUSTOM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
 
 def custom_sensor_schema(
     message: int,
-    unit_of_measurement: str = sensor._UNDEF,
-    icon: str = sensor._UNDEF,
-    accuracy_decimals: int = sensor._UNDEF,
-    device_class: str = sensor._UNDEF,
-    state_class: str = sensor._UNDEF,
-    entity_category: str = sensor._UNDEF,
+    unit_of_measurement: str = sensor.cv.UNDEFINED,
+    icon: str = sensor.cv.UNDEFINED,
+    accuracy_decimals: int = sensor.cv.UNDEFINED,
+    device_class: str = sensor.cv.UNDEFINED,
+    state_class: str = sensor.cv.UNDEFINED,
+    entity_category: str = sensor.cv.UNDEFINED,
     raw_filters=[],
 ):
     return sensor.sensor_schema(
